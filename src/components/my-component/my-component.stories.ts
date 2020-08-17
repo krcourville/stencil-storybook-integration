cf5444f262821825835866eccaf45f438c298eab
Ken Courville <ken.courville@dhigroupinc.com>
2020-08-17 07:38:55 -0600

    move story next to component

---
+++ src/components/my-component/my-component.stories.ts
@@ -0,0 +1,9 @@
+import {} from "@storybook/addon-controls";
+
+export default {
+  title: "Components/My Component",
+};
+
+export const Default = () => `
+    <my-component first="John" last="Smith"></my-component>
+`;
--- src/stories/StencilTest.stories.js
+++
@@ -1,8 +0,0 @@
-export default {
-  title: "Stencil/ComponentTest",
-};
-
-export const MyComponent = () => `
-  <h4>My Component Test</h4>
-  <my-component first="John" middle="R" last="Smith"></my-component>
-`;
